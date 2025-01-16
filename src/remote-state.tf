1079f6232fed4c733159e09c042f06f4dba6f4bd
Igor Rodionov <goruha@gmail.com>
2025-01-16 03:47:21 +0100

    Added tests

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -1,6 +1,6 @@
 module "aurora_postgres" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
-  version = "1.5.0"
+  version = "1.8.0"
 
   component = var.aurora_postgres_component_name
 
